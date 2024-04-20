7011e3961c1a94763174e465eae7026c459ea017
Crowdbotics <team@crowdbotics.com>
2024-04-20 17:58:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-band-47650",
+  url: "https://floral-band-47650.botics.co",
+  api: "https://floral-band-47650.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
